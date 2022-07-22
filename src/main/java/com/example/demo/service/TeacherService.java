988fe47a3842140ce4e2a1d02c0a1afb8f13f919
simonkrupa <simon.krupa@icloud.com>
2022-07-22 17:03:57 +0200

    implement: teacher responses

--- src/main/java/com/example/demo/service/TeacherService.java
+++ src/main/java/com/example/demo/service/TeacherService.java
@@ -2,6 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.domain.Teacher;
 import com.example.demo.domain.TeacherRepository;
+import com.example.demo.exceptions.BadRequestException;
+import com.example.demo.exceptions.NotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,18 +17,24 @@ public class TeacherService {
         this.repository = repository;
     }
 
-    public Teacher createTeacher(Teacher teacher){ return repository.save(teacher);}
+    public Teacher createTeacher(Teacher teacher) {
+        try {
+            return repository.save(teacher);
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException();
+        }
+    }
 
     public List<Teacher> getAllTeachers(){
         return repository.findAll();
     }
 
     public Teacher getById(Long id){
-        return repository.getById(id);
+        return repository.findById(id).orElseThrow(NotFoundException::new);
     }
 
-    public boolean deleteTeacher(Teacher teacher){
-        repository.delete(teacher);
-        return !repository.existsById(teacher.getId());
+    public boolean deleteTeacher(Long id){
+        repository.deleteById(id);
+        return !repository.existsById(id);
     }
 }
--- src/main/java/com/example/demo/web/TeacherController.java
+++ src/main/java/com/example/demo/web/TeacherController.java
@@ -2,8 +2,10 @@ package com.example.demo.web;
 
 import com.example.demo.domain.Teacher;
 import com.example.demo.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.standard.Media;
@@ -16,36 +18,32 @@ public class TeacherController {
 
     private final TeacherService teacherService;
 
+    @Autowired
     public TeacherController(TeacherService teacherService) {
         this.teacherService = teacherService;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Teacher> getAll(){
-        return teacherService.getAllTeachers();
+    public ResponseEntity<List<Teacher>> getAll(){
+        return new ResponseEntity<>(teacherService.getAllTeachers(), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Teacher getById(@PathVariable("id") Long id){
-        return teacherService.getById(id);
+    public ResponseEntity<Teacher> getById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(teacherService.getById(id), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{id}")
-    public void delete(@PathVariable("id") Long id, HttpServletResponse response){
-        Teacher teacher = teacherService.getById(id);
-        if(teacher == null){
-            response.setStatus(HttpStatus.NOT_FOUND.value());
-            return;
-        }
-        boolean success = teacherService.deleteTeacher(teacher);
-        if(!success){
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+        if(this.teacherService.deleteTeacher(id)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Teacher create(@RequestBody TeacherDto body){
-        Teacher teacher = new Teacher(body.getFirstName(), body.getLastName(), body.getEmail(), body.getCourses());
-        return teacherService.createTeacher(teacher);
+    public ResponseEntity<Teacher> create(@RequestBody TeacherDto body){
+        return new ResponseEntity<>(this.teacherService.createTeacher(new Teacher(body.getFirstName(), body.getLastName(), body.getEmail(), body.getCourses())), HttpStatus.CREATED);
     }
 }
